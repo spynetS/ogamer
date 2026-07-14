182e8473160d25459ee67dd4b4b78eefba46d5c5
spynets <alfred@stensatter.se>
2026-07-14 20:13:41 +0200

    removed map stuff

--- game2/Trees.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="Trees" tilewidth="64" tileheight="96" tilecount="2" columns="2">
- <image source="../game/assets/FREE_Fantasy Forest/Tiles/Trees.png" width="128" height="96"/>
-</tileset>
